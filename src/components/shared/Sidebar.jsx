6a6ee3708f81354c751dc346f126f33b3021ccae
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2024-05-21 16:12:27 -0600

    avance

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -4,6 +4,7 @@ import { Link, useLocation } from 'react-router-dom'
 import { FcBullish } from 'react-icons/fc'
 import { HiOutlineLogout } from 'react-icons/hi'
 import { DASHBOARD_SIDEBAR_LINKS, DASHBOARD_SIDEBAR_BOTTOM_LINKS } from '../../lib/constants'
+import SidebarLinkGroup from './SidebarLinkGroup';
 
 const linkClass =
 	'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
