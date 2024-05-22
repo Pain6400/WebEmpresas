516d29fc9adae4f37aeba4c498a09370db972de1
Pain6400 <45575989+Pain6400@users.noreply.github.com>
2024-05-21 21:32:15 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/shared/Layout'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
+import Login from './pages/Account/login'
 
 function App() {
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handleLogin = () => {
+        setIsAuthenticated(false);
+    };
+
+    const handleLogout = () => {
+        setIsAuthenticated(false);
+    };
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    <Route index element={<Dashboard />} />
+                    <Route
+                        path="/"
+                        element={isAuthenticated ? <Dashboard onLogout={handleLogout} /> : <Navigate to="/login" />}
+                    />
                     <Route path="products" element={<Products />} />
                 </Route>
                 <Route path="/register" element={<Register />} />
+                <Route
+                    path="/login"
+                    element={isAuthenticated ? <Login onLogin={handleLogin} /> : <Navigate to="/" />}
+                />
             </Routes>
         </Router>
     )
